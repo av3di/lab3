0c821be4c0f87ea78f7c9085b249b5dd8a47ea39
Evonee Uribe <ekuribe@ucsd.edu>
2015-03-10 10:40:48 +0000

    Landing Page 1

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -19,29 +19,6 @@ function initializePage() {
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 	$("a.thumbnail").click(projectClick);
-	function projectClick(e) {
-		//console.log("Project clicked");
-
-		// prevent the page from reloading
-		e.preventDefault();
-
-		// In an event handler, $(this) refers to 
-		// the object that triggered the event
-		var projectTitle = $(this).find("p").text();
-		var jumbotronHeader = $(".jumbotron h1");
-		jumbotronHeader.text(projectTitle);
-		//$(this).css("background-color", "#7fff00");
-		var containingProject = $(this).closest(".project");
-		var description = $(containingProject).find(".project-description");
-		if(description.length == 0){
-			$("containingProject").append("<div class='project-description'><p>Description of the project.</p></div>");
-		} else {
-			//description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
-			$(".project-description").fadeOut();
-		}
-		containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
-
-	}
 }
 
 function updateProject(e) {
@@ -51,4 +28,32 @@ function updateProject(e) {
 	});
 	var newText = $("#description").val();
 	$(projectID + " .project-description").text(newText);
+}
+
+function projectClick(e) {
+	//console.log("Project clicked");
+
+	// prevent the page from reloading
+	e.preventDefault();
+
+	// In an event handler, $(this) refers to 
+	// the object that triggered the event
+	var projectTitle = $(this).find("p").text();
+	var jumbotronHeader = $(".jumbotron h1");
+	jumbotronHeader.text(projectTitle);
+	//$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	
+	if(description.length == 0) 
+	{
+		$("containingProject").append("<div class='project-description'><p>Description of the project.</p></div>");
+	} 
+	else 
+	{
+		//description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+		$(".project-description").fadeOut();
+	}
+	containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+
 }
